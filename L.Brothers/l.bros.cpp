68bc8b10848d93c32e5b00a8963627a4c9b71039
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-19 20:54:27 +0000

    GOOD day

---
+++ L.Brothers/l.bros.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+
+using namespace std ;
+
+
+int main(){
+
+
+string name11,name12,name21,name22;
+
+cin>>name11;
+cin>>name12;
+cin>>name21;
+cin>>name22;
+
+if (name12==name22){
+cout<<"ARE Brothers"<<endl;
+
+}else{
+cout<<"NOT"<<endl;
+
+}
+
+
+
+}
