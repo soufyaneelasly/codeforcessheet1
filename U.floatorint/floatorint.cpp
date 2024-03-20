4bdf7a638a2fc43614892d52739e4d056609922a
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-20 19:58:03 +0000

    U

---
+++ U.floatorint/floatorint.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+
+
+float n;
+
+cin>>n;
+
+
+if (int(n)-n==0.0){
+cout<<"int "<<int(n)<<endl;
+}else{
+cout<<"float "<<int(n);
+cout<<" "<<n-int(n)<<endl;
+
+}
+}
