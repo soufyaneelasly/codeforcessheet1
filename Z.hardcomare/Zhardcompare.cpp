7bce05b7fb029f5e9aa9b9b30ef191c02149ead6
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-21 22:44:52 +0000

    Z

---
+++ Z.hardcomare/Zhardcompare.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
+
+
+int main(){
+
+
+double a,b,c,d,x,z,y;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+
+    y=b/d;
+
+z=pow(a,y)/c;
+    if(z>1){
+cout<<"YES"<<endl;
+}else{
+
+cout<<"NO"<<endl;
+            }
+}
